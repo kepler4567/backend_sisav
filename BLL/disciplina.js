b9ba5c2d0562683c35e0459c50e8b12222fa811d
kepler4567 <kepler.batista456@gmail.com>
2024-08-25 13:58:48 -0300

    adicionado media disciplina

--- BLL/disciplina.js
+++ BLL/disciplina.js
@@ -35,6 +35,42 @@ router.get('/horarios/:ra_aluno', (req, res) => {
     });
 });
 
+router.get('/media/:ra_aluno', (req, res) => {
+    const { ra_aluno } = req.params;
+
+
+    const sql = `SELECT 
+                    d.id_disciplina, 
+                    d.situacao,
+                    d.faltas,
+                    d.media,
+                    d.nome_disciplina
+                FROM 
+                    disciplina d
+                JOIN 
+                    usuario u ON d.id_usuario = u.ra
+                WHERE 
+                    u.ra = ?;`;
+
+    db.all(sql, [ra_aluno], (err, rows) => {  // Alterado de db.get para db.all
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        if (rows.length === 0) {  // Verifica se o array de resultados está vazio
+            res.status(404).json({ "message": "Nenhuma disciplina encontrada para este aluno" });
+            return;
+        }
+        res.json({
+            "message": "Disciplinas encontradas",
+            "data": rows  // Retorna todas as linhas encontradas
+        });
+    });
+});
+
+
+
+
 router.get('/', (req, res) => {
     const { ra_aluno } = req.body;
 
--- init-db.js
+++ init-db.js
@@ -22,6 +22,9 @@ db.serialize(() => {
           dia_semana TEXT NOT NULL,
           horario INT NOT NULL,
           id_usuario INT,
+          situacao TEXT,
+          faltas INT,
+          media INT,
           FOREIGN KEY (id_usuario) REFERENCES usuario(ra)  -- Adiciona a chave estrangeira
       );`);
 
@@ -44,21 +47,21 @@ db.serialize(() => {
           (104, 'Ana Costa', 'ana.costa@email.com', 'senha101112');`);
   
       // Inserindo dados na tabela disciplina
-      db.run(`INSERT INTO disciplina (id_disciplina, nome_disciplina, dia_semana, horario, id_usuario) VALUES 
-          (201, 'Matemática', 'Segunda-feira', 1, 101),
-          (202, 'História', 'Terça-feira',2, 101),
-          (203, 'Geografia', 'Quarta-feira',2, 101),
-          (204, 'Química', 'Quinta-feira',2, 101),
-          (205, 'Física', 'Sexta-feira',2, 101),
-          (206, 'Biologia', 'Segunda-feira',2, 101),
-          (207, 'Inglês', 'Quarta-feira',1, 101),
-          (208, 'Educação Física', 'Sexta-feira',1, 101);`);
+      db.run(`INSERT INTO disciplina (id_disciplina, nome_disciplina, dia_semana, horario, id_usuario, situacao, faltas, media) VALUES 
+          (201, 'Matemática', 'Segunda-feira', 1, 101, 'reprovado', 3, 50),
+          (202, 'História', 'Terça-feira',2, 101, 'aprovado', 5, 70),
+          (203, 'Geografia', 'Quarta-feira',2, 101, 'em andamento', 4, null),
+          (204, 'Química', 'Quinta-feira',2, 101, 'aprovado', 1, 66),
+          (205, 'Física', 'Sexta-feira',2, 101, 'aprovado', 2, 80),
+          (206, 'Biologia', 'Segunda-feira',2, 101, 'em andamento', 4, null),
+          (207, 'Inglês', 'Quarta-feira',1, 101, 'aprovado', 2, 70),
+          (208, 'Educação Física', 'Sexta-feira',1, 101, 'reprovado', 1, 44);`);
 
     db.run(`INSERT INTO AAC (id_aac, nome, horas, ano, status, ra_aluno) VALUES 
     (301, 'Projeto de Extensão', 30, 2023, 'Concluído', 101),
-    (302, 'Curso de Verão', 20, 2024, 'Em Andamento', 101),
+    (302, 'Curso de Verão', 20, 2024, 'Pendente', 101),
     (303, 'Voluntariado', 40, 2023, 'Concluído', 102),
-    (304, 'Participação em Congresso', 10, 2024, 'Em Andamento', 103),
+    (304, 'Participação em Congresso', 10, 2024, 'Pendente', 103),
     (305, 'Oficina de Capacitação', 15, 2023, 'Concluído', 104);`);
 });
   
